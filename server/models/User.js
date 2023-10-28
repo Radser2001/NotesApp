8120567ad3583220becf18d2a93bc22ae3138c81
Radser2001 <supuni.randeniya@gmail.com>
2023-10-28 10:51:25 +0530

    Add db connection

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  //role can be more than one value
+  roles: [
+    {
+      type: String,
+      default: "Employee",
+    },
+  ],
+  active: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
